0a54fade53d848b665e0abba64fde9f12e477f72
ahmed0ty <ahmedabdelbaky2321@gmail.com>
2026-06-12 14:08:59 +0300

    fix next config remotePatterns

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "res.cloudinary.com",
-      "lh3.googleusercontent.com", 
-      "avatars.githubusercontent.com"
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
     ],
   },
 };
